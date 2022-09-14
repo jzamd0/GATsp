ed96ae11d0d2e5ac27ad6bf05b732d24ff5bd675
jonathan <email@example.com>
2022-09-13 20:33:50 -0700

    Add GUI classes from App to its own folder

---
+++ App/gui/FrmMain.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Forms;
+
+namespace App
+{
+    public partial class FrmMain : Form
+    {
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+    }
+}

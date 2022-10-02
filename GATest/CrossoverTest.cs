648d48c11aae4df8b46a3df31a6111d763bc1f3a
jonathan <email@example.com>
2022-10-01 22:22:42 -0700

    Add Test project to solution
    - Add unit tests for the genetic algorithm.
    - Add tests for crossover, mutation and selection operators.

---
+++ GATest/CrossoverTest.cs
@@ -0,0 +1,110 @@
+﻿using Lib.Genetics.Operators;
+using NUnit.Framework;
+
+namespace GATest
+{
+    public class CrossoverTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void PPXTestFirstParentToSecondParent()
+        {
+            var xop = new Crossover();
+
+            var mask = new int[] { 1, 0, 0, 0, 1, 1, 0, 1, 1 };
+            var parent1 = new double[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            var parent2 = new double[] { 5, 4, 6, 3, 1, 9, 2, 7, 8 };
+            var expected = new double[] { 1, 5, 4, 6, 2, 3, 9, 7, 8 };
+            var size = expected.Length;
+
+            var actual = xop.PPX(parent1, parent2, size, mask);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void PPXTestSecondParentToFirstParent()
+        {
+            var xop = new Crossover();
+
+            var mask = new int[] { 1, 0, 0, 0, 1, 1, 0, 1, 1 };
+            var parent1 = new double[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            var parent2 = new double[] { 5, 4, 6, 3, 1, 9, 2, 7, 8 };
+            var expected = new double[] { 5, 1, 2, 3, 4, 6, 7, 9, 8 };
+            var size = expected.Length;
+
+            var actual = xop.PPX(parent2, parent1, size, mask);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void OBXTestFirstParentToSecondParent()
+        {
+            var xop = new Crossover();
+
+            var mask = new int[] { 1, 0, 0, 0, 1, 1, 0, 1, 1 };
+            var parent1 = new double[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            var parent2 = new double[] { 5, 4, 6, 3, 1, 9, 2, 7, 8 };
+            var expected = new double[] { 1, 4, 3, 2, 5, 6, 7, 8, 9 };
+            var size = expected.Length;
+
+            var actual = xop.OBX(parent1, parent2, size, mask);
+
+            Assert.AreEqual(expected, actual);
+        }
+        [Test]
+        public void OBXTestSecondParentToFirstParent()
+        {
+            var xop = new Crossover();
+
+            var mask = new int[] { 1, 0, 0, 0, 1, 1, 0, 1, 1 };
+            var parent1 = new double[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            var parent2 = new double[] { 5, 4, 6, 3, 1, 9, 2, 7, 8 };
+            var expected = new double[] { 5, 2, 3, 4, 1, 9, 6, 7, 8 };
+            var size = expected.Length;
+
+            var actual = xop.OBX(parent2, parent1, size, mask);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void TPXTestFirstParentToSecondParent()
+        {
+            var xop = new Crossover();
+
+            var point1 = 3;
+            var point2 = 7;
+            var parent1 = new double[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            var parent2 = new double[] { 5, 4, 6, 3, 1, 9, 2, 7, 8 };
+            var expected = new double[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            var size = expected.Length;
+
+            var actual = xop.TPX(parent1, parent2, size, point1, point2);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void TPXTestSecondParentToFirstParent()
+        {
+            var xop = new Crossover();
+
+            var point1 = 3;
+            var point2 = 7;
+            var parent1 = new double[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            var parent2 = new double[] { 5, 4, 6, 3, 1, 9, 2, 7, 8 };
+            var expected = new double[] { 5, 4, 6, 3, 1, 2, 9, 7, 8 };
+            var size = expected.Length;
+
+            var actual = xop.TPX(parent2, parent1, size, point1, point2);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}

9f09e31684f436cca836b3a6591f3030e76ff281
jonathan <email@example.com>
2022-10-15 00:38:23 -0700

    Add unit test to mutation operator insert with point 1 before point 2

--- GATest/MutationTest.cs
+++ GATest/MutationTest.cs
@@ -40,12 +40,26 @@ namespace GATest
         }
 
         [Test]
-        public void InsertTest()
+        public void InsertPoint1AfterPoint2Test()
         {
             var values = new double[] { 1, 4, 7, 2, 5, 8, 3, 6, 9 };
             var expected = new double[] { 1, 4, 6, 7, 2, 5, 8, 3, 9 };
             var point1 = 7;
             var point2 = 2;
+            var size = expected.Length;
+
+            var actual = Mutation.Insert(values, size, point1, point2);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void InsertPoint1BeforePoint2Test()
+        {
+            var values = new double[] { 1, 4, 7, 2, 5, 8, 3, 6, 9 };
+            var expected = new double[] { 1, 4, 2, 5, 8, 3, 6, 7, 9 };
+            var point1 = 2;
+            var point2 = 7;
             var size = expected.Length;
 
             var actual = Mutation.Insert(values, size, point1, point2);
